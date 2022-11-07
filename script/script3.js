7d1d14de8ea15df4b35c84abf9c655396d524259
unknown <mhamd.naser.edden@gmail.com>
2022-11-07 15:03:53 +0200

    JaveScript>

--- script/script3.js
+++ script/script3.js
@@ -1,9 +1,4 @@
 function test_zero(){
-    let name = document.getElementById("demo3").innerHTML ;
-    document.getElementById("demo3").innerHTML = name.replace("name set", "muhammed naser");
-}
-
-function test_one(){
     let first = "Muhammed";
     let last = "naser";
 
@@ -11,28 +6,33 @@ function test_one(){
     let resli = last.slice(-2);
     let str = first.substring(2,7);
 
-    document.getElementById("demo4").innerHTML = len + ' ' + resli  + ' ' + str;
+    document.getElementById("demo3").innerHTML = len + ' ' + resli  + ' ' + str;
+}
+
+function test_one(){
+    let name =  "my name is : name set" ;
+    document.getElementById("demo3").innerHTML = name.replace("name set", "muhammed naser");
 }
 
 function test_two(){
     let first = "this my project name this the first pro for name ";
-    document.getElementById("demo4").innerHTML = first.replace("name", "Muhammed");
+    document.getElementById("demo3").innerHTML = first.replaceAll("name", "Muhammed");
 
 }
 
 function test_three(){
     let first = "Muhammed";
-    document.getElementById("demo4").innerHTML =  first.toUpperCase();
+    document.getElementById("demo3").innerHTML =  first.toUpperCase();
 }
 
 function test_four(){
     let first = "MUHAMMED";
-    document.getElementById("demo4").innerHTML =  first.toLowerCase();
+    document.getElementById("demo3").innerHTML =  first.toLowerCase();
 }
 
 function test_five(){
     let first = "Muhammed";
     let last = "Naser";
     let full = first.concat(" "+last);
-    document.getElementById("demo4").innerHTML =  full ;
+    document.getElementById("demo3").innerHTML =  full ;
 }
